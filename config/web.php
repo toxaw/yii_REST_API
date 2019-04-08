3b17071f13cf519cc252d21d77cf7c0b334cd813
toxaw <antonser2@gmail.com>
2019-04-08 17:30:03 +0300

    создал htaccessы + подкорректировал config/web.php + yii теперьв корне htdocs

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'tQomP_drxOwUQuacbN0pItxN2lwjen64',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+       
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
